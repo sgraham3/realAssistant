936937d2a7104f497e92d8d366e37d12b5e501db
sgraham3@gmail.com <sgraham3@gmail.com>
2025-09-03 14:41:48 -0400

    modified:   buildexe.bat 	modified:   dist/realAssistant.exe 	deleted:    realAssistant.py 	modified:   realAssistant.spec 	modified:   requirements.txt 	actions/ 	dist/realAssistant-signed.exe 	dist/rptscert.pem 	dist/rptskey.pem 	dist/rptsrequest.csr 	dist/sign.bat 	dist/sign.txt 	gui.py 	main.py 	sign.txt 	signexe.bat 	utils/

--- buildexe.bat
+++ buildexe.bat
@@ -1,2 +1,2 @@
-pyinstaller --name "realAssistant" --onefile --windowed --add-data "metadata.ini;." --icon "icon.ico" realAssistant.py
+pyinstaller --name "realAssistant" --onefile --windowed --add-data "metadata.ini;." --icon "icon.ico" main.py
 rem pyinstaller --clean realAssistant.spec
--- realAssistant.spec
+++ realAssistant.spec
@@ -2,7 +2,7 @@
 
 
 a = Analysis(
-    ['realAssistant.py'],
+    ['main.py'],
     pathex=[],
     binaries=[],
     datas=[('metadata.ini', '.')],
